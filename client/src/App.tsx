f06c9521a8e4625a5d020ac253533b3739d9562d
@BennEntterprise <kyle.bennett@betweenfiveandnine.com>
2022-01-01 20:59:22 -0500

    add basic typescript to the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react'
+const logo = require('./logo.svg')
+
+interface Todo {
+  id: string;
+  task: string;
+}
+
+interface EndpointResponse {
+  todos: Todo[]
+}
 
 function App() {
   const [connectionToBackend, setConnectionToBackend] = useState('')
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState<Todo[]>([])
 
   useEffect(() => {
     fetch('/api').then(res => res.json()).then((data => {
@@ -12,7 +21,7 @@ function App() {
       setConnectionToBackend(data.message)
     })).catch(e => console.log(e))
 
-    fetch('/api/todos').then(res => res.json()).then((data => {
+    fetch('/api/todos').then(res => res.json()).then(((data: EndpointResponse) => {
       console.log(data)
       setTodos(data.todos)
     })).catch(e => console.log(e))
@@ -23,7 +32,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Connected to backend?: {connectionToBackend}
+          Checking Connection to Backend: {connectionToBackend}
         </p>
         <p>MongoConnection to Todos?: (should see todo's below)</p>
         <ul>{todos.map((todo, i) => <li key={i}>{todo.task}</li>)}</ul>
